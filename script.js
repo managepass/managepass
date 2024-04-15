0d20bab47a5e63bfae7cd0e4e24c289eb9c980c9
managepass <166999116+managepass@users.noreply.github.com>
2024-04-15 17:08:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,55 @@
-function Strength(password, commonPasswords) {
-  let i = 0;
-  if (password.length > 6) {
-    i++;
+function Strength(password) {
+    let i = 0;
+    if (password.length > 6) {
+      i++;
+    }
+    if (password.length >= 10) {
+      i++;
+    }
+  
+    if (/[A-Z]/.test(password)) {
+      i++;
+    }
+  
+    if (/[0-9]/.test(password)) {
+      i++;
+    }
+  
+    if (/[A-Za-z0-8]/.test(password)) {
+      i++;
+    }
+  
+    return i;
   }
-  if (password.length >= 10) {
-    i++;
-  }
-
-  if (/[A-Z]/.test(password)) {
-    i++;
-  }
-
-  if (/[0-9]/.test(password)) {
-    i++;
-  }
-
-  if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-    i++;
-  }
-
-  // Check if password is common
-  if (commonPasswords.includes(password)) {
-    i = 0; // Consider common passwords as weak regardless of other criteria
-  }
-
-  return i;
-}
-
-let commonPasswords = [];
-// Fetch the list of common passwords from the server
-fetch('passwords.txt')
-  .then(response => response.text())
-  .then(data => {
-    commonPasswords = data.split('\n'); // Assuming each password is on a new line
-  })
-  .catch(error => {
-    console.error('Error fetching the passwords file:', error);
+  
+  let container = document.querySelector(".container");
+  document.addEventListener("keyup", function (e) {
+    let password = document.querySelector("#YourPassword").value;
+  
+    let strength = Strength(password);
+    if (strength <= 2) {
+      container.classList.add("weak");
+      container.classList.remove("moderate");
+      container.classList.remove("strong");
+    } else if (strength >= 2 && strength <= 4) {
+      container.classList.remove("weak");
+      container.classList.add("moderate");
+      container.classList.remove("strong");
+    } else {
+      container.classList.remove("weak");
+      container.classList.remove("moderate");
+      container.classList.add("strong");
+    }
   });
-
-let container = document.querySelector(".container");
-document.addEventListener("keyup", function (e) {
-  let password = document.querySelector("#YourPassword").value;
-
-  let strength = Strength(password, commonPasswords);
-  if (strength <= 2) {
-    container.classList.add("weak");
-    container.classList.remove("moderate");
-    container.classList.remove("strong");
-  } else if (strength > 2 && strength <= 4) {
-    container.classList.remove("weak");
-    container.classList.add("moderate");
-    container.classList.remove("strong");
-  } else {
-    container.classList.remove("weak");
-    container.classList.remove("moderate");
-    container.classList.add("strong");
-  }
-});
-
-let password = document.querySelector("#YourPassword");
-let show = document.querySelector(".show");
-show.onclick = function () {
-  if (password.type === "password") {
-    password.setAttribute("type", "text");
-    show.classList.add("hide");
-  } else {
-    password.setAttribute("type", "password");
-    show.classList.remove("hide");
-  }
-};
+  
+  let password = document.querySelector("#YourPassword");
+  let show = document.querySelector(".show");
+  show.onclick = function () {
+    if (password.type === "password") {
+      password.setAttribute("type", "text");
+      show.classList.add("hide");
+    } else {
+      password.setAttribute("type", "password");
+      show.classList.remove("hide");
+    }
+  };
